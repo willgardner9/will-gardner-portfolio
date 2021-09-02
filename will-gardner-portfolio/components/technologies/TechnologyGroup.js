691bc8310616faa70001e841869b5a789dcfc0b7
willgardner9 <willgardner9@gmail.com>
2021-09-02 10:47:45 +0100

    Hide scrollbars on windows

--- will-gardner-portfolio/components/technologies/TechnologyGroup.js
+++ will-gardner-portfolio/components/technologies/TechnologyGroup.js
@@ -1,20 +1,32 @@
-import Image from 'next/image'
-import ReactTooltip from 'react-tooltip';
+import Image from "next/image";
+import ReactTooltip from "react-tooltip";
 
-const TechnologyGroup = ({ technologyGroupText, technologiesObject }) => {
+const TechnologyGroup = ({technologyGroupText, technologiesObject}) => {
   return (
-    <section className="flex flex-col mr-0 sm:mr-20">
-      <h5 className="mt-16 uppercase text-xs font-medium text-gray-500">{ technologyGroupText }</h5>
+    <section className="flex flex-col mr-0 sm:mr-20 webkit-hide-scrollbar">
+      <h5 className="mt-16 uppercase text-xs font-medium text-gray-500">
+        {technologyGroupText}
+      </h5>
       <div className="flex flex-row mt-4 mb-4 w-95vw sm:w-full overflow-x-scroll">
-        { technologiesObject.map((technology) =>
-          <div key={technology.tooltip} className="flex flex-col items-center justify-center mt-2 sm:mt-0 mr-8 min-w-max" onMouseEnter={() => console.log('hi')}>
-            <Image data-tip={technology.tooltip} src={`/${technology.url}.svg`} width={technology.enlarge ? '80' : '30'} height="30" alt={`${technology.tooltip} logo`} />
+        {technologiesObject.map((technology) => (
+          <div
+            key={technology.tooltip}
+            className="flex flex-col items-center justify-center mt-2 sm:mt-0 mr-8 min-w-max"
+            onMouseEnter={() => console.log("hi")}
+          >
+            <Image
+              data-tip={technology.tooltip}
+              src={`/${technology.url}.svg`}
+              width={technology.enlarge ? "80" : "30"}
+              height="30"
+              alt={`${technology.tooltip} logo`}
+            />
           </div>
-        ) }
+        ))}
       </div>
-      <ReactTooltip place="bottom" type="dark" effect="solid"/>
+      <ReactTooltip place="bottom" type="dark" effect="solid" />
     </section>
-  )
-}
+  );
+};
 
-module.exports = TechnologyGroup
+module.exports = TechnologyGroup;
--- will-gardner-portfolio/pages/_app.js
+++ will-gardner-portfolio/pages/_app.js
@@ -1,12 +1,13 @@
-import 'tailwindcss/tailwind.css'
-import { ThemeProvider } from 'next-themes'
+import "tailwindcss/tailwind.css";
+import {ThemeProvider} from "next-themes";
+import "../components/technologies/scrollbar.css";
 
-function MyApp({ Component, pageProps }) {
-  return(
+function MyApp({Component, pageProps}) {
+  return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
