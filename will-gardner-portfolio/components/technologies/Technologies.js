8d33031aead91164d5fd653010cc121d42cfab2d
willgardner9 <willgardner9@gmail.com>
2021-09-02 10:25:08 +0100

    Update testing tools

--- will-gardner-portfolio/components/technologies/Technologies.js
+++ will-gardner-portfolio/components/technologies/Technologies.js
@@ -1,207 +1,242 @@
-const TechnologyGroup = require("./TechnologyGroup")
+const TechnologyGroup = require("./TechnologyGroup");
 
 const Technologies = () => {
-
   const languagesObject = [
     {
-      url: 'html-5',
-      tooltip: 'HTML'
+      url: "html-5",
+      tooltip: "HTML",
     },
     {
-      url: 'css-3',
-      tooltip: 'CSS'
+      url: "css-3",
+      tooltip: "CSS",
     },
     {
-      url: 'javascript',
-      tooltip: 'JavaScript'
+      url: "javascript",
+      tooltip: "JavaScript",
     },
     {
-      url: 'typescript-icon',
-      tooltip: 'TypeScript'
+      url: "typescript-icon",
+      tooltip: "TypeScript",
     },
     {
-      url: 'nodejs',
-      tooltip: 'Node.js',
+      url: "nodejs",
+      tooltip: "Node.js",
       enlarge: true,
     },
-  ]
+  ];
 
   const versionControlObject = [
     {
-      url: 'git',
-      tooltip: 'Git',
+      url: "git",
+      tooltip: "Git",
       enlarge: true,
     },
     {
-      url: 'github-icon',
-      tooltip: 'GitHub'
+      url: "github-icon",
+      tooltip: "GitHub",
     },
-  ]
+  ];
 
   const testingToolsObject = [
     {
-      url: 'jest',
-      tooltip: 'Jest'
+      url: "jest",
+      tooltip: "Jest",
     },
-    {
-      url: 'jasmine',
-      tooltip: 'Jasmine'
-    },
-  ]
+    // {
+    //   url: 'jasmine',
+    //   tooltip: 'Jasmine'
+    // },
+  ];
 
   const frontEndFrameworksObject = [
     {
-      url: 'react',
-      tooltip: 'React'
+      url: "react",
+      tooltip: "React",
     },
     {
-      url: 'nextjs',
-      tooltip: 'Next.js',
+      url: "nextjs",
+      tooltip: "Next.js",
       enlarge: true,
     },
     {
-      url: 'vue',
-      tooltip: 'Vue'
+      url: "vue",
+      tooltip: "Vue",
     },
     {
-      url: 'nuxt',
-      tooltip: 'Nuxt.js',
+      url: "nuxt",
+      tooltip: "Nuxt.js",
       enlarge: true,
     },
     {
-      url: 'svelte',
-      tooltip: 'Svelte',
+      url: "svelte",
+      tooltip: "Svelte",
       enlarge: true,
     },
     {
-      url: 'tailwindcss',
-      tooltip: 'Tailwind CSS',
+      url: "tailwindcss",
+      tooltip: "Tailwind CSS",
       enlarge: true,
     },
-  ]
+  ];
 
   const backEndFrameworksObject = [
     {
-      url: 'express',
-      tooltip: 'Express',
+      url: "express",
+      tooltip: "Express",
       enlarge: true,
     },
     {
-      url: 'nestjs',
-      tooltip: 'NestJS'
+      url: "nestjs",
+      tooltip: "NestJS",
     },
-  ]
+  ];
 
   const databasesObjects = [
     {
-      url: 'mongodb',
-      tooltip: 'MongoDB',
+      url: "mongodb",
+      tooltip: "MongoDB",
       enlarge: true,
     },
     {
-      url: 'postgresql',
-      tooltip: 'PostgreSQL'
+      url: "postgresql",
+      tooltip: "PostgreSQL",
     },
-  ]
+  ];
 
   const externalAPIObject = [
     {
-      url: 'puppeteer',
-      tooltip: 'Puppeteer',
+      url: "puppeteer",
+      tooltip: "Puppeteer",
     },
     {
-      url: 'stripe',
-      tooltip: 'Stripe',
+      url: "stripe",
+      tooltip: "Stripe",
       enlarge: true,
     },
     {
-      url: 'supabase',
-      tooltip: 'supabase',
+      url: "supabase",
+      tooltip: "supabase",
       enlarge: true,
-    }
-  ]
+    },
+  ];
 
   const devToolsObject = [
     {
-      url: 'postman',
-      tooltip: 'Postman',
+      url: "postman",
+      tooltip: "Postman",
       enlarge: true,
     },
     {
-      url: 'insomnia',
-      tooltip: 'Insomnia',
+      url: "insomnia",
+      tooltip: "Insomnia",
     },
     {
-      url: 'npm',
-      tooltip: 'NPM',
+      url: "npm",
+      tooltip: "NPM",
     },
-  ]
+  ];
 
   const deploymentToolsObject = [
     {
-      url: 'digital-ocean',
-      tooltip: 'Digital Ocean',
+      url: "digital-ocean",
+      tooltip: "Digital Ocean",
       enlarge: false,
     },
     {
-      url: 'vercel',
-      tooltip: 'Vercel',
+      url: "vercel",
+      tooltip: "Vercel",
       enlarge: true,
     },
     {
-      url: 'heroku',
-      tooltip: 'Heroku',
+      url: "heroku",
+      tooltip: "Heroku",
       enlarge: true,
     },
-  ]
+  ];
 
   const CMSOBject = [
     {
-      url: 'prismic',
-      tooltip: 'Prismic',
+      url: "prismic",
+      tooltip: "Prismic",
       enlarge: true,
     },
     {
-      url: 'strapi',
-      tooltip: 'Strapi',
+      url: "strapi",
+      tooltip: "Strapi",
       enlarge: true,
     },
-  ]
+  ];
 
   const designObject = [
     {
-      url: 'figma',
-      tooltip: 'Figma',
+      url: "figma",
+      tooltip: "Figma",
     },
-  ]
+  ];
 
   const learningRoadmapObject = [
     {
-      url: 'go',
-      tooltip: 'Go',
+      url: "go",
+      tooltip: "Go",
     },
     {
-      url: 'graphql',
-      tooltip: 'GraphQL',
+      url: "graphql",
+      tooltip: "GraphQL",
     },
-  ]
+  ];
 
   return (
     <section className="flex sm:flex-row flex-col flex-wrap">
-      <TechnologyGroup technologyGroupText="Languages" technologiesObject={languagesObject} />
-      <TechnologyGroup technologyGroupText="Front end frameworks" technologiesObject={frontEndFrameworksObject} />
-      <TechnologyGroup technologyGroupText="Back end frameworks" technologiesObject={backEndFrameworksObject} />
-      <TechnologyGroup technologyGroupText="Databases" technologiesObject={databasesObjects} />
-      <TechnologyGroup technologyGroupText="Testing tools" technologiesObject={testingToolsObject} />
-      <TechnologyGroup technologyGroupText="External APIs" technologiesObject={externalAPIObject} />
-      <TechnologyGroup technologyGroupText="Dev tools" technologiesObject={devToolsObject} />
-      <TechnologyGroup technologyGroupText="Deployment tools" technologiesObject={deploymentToolsObject} />
-      <TechnologyGroup technologyGroupText="Version control" technologiesObject={versionControlObject} />
-      <TechnologyGroup technologyGroupText="CMS" technologiesObject={CMSOBject} />
-      <TechnologyGroup technologyGroupText="Design" technologiesObject={designObject} />
-      <TechnologyGroup technologyGroupText="Learning roadmap" technologiesObject={learningRoadmapObject} />
+      <TechnologyGroup
+        technologyGroupText="Languages"
+        technologiesObject={languagesObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="Front end frameworks"
+        technologiesObject={frontEndFrameworksObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="Back end frameworks"
+        technologiesObject={backEndFrameworksObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="Databases"
+        technologiesObject={databasesObjects}
+      />
+      <TechnologyGroup
+        technologyGroupText="Testing tools"
+        technologiesObject={testingToolsObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="External APIs"
+        technologiesObject={externalAPIObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="Dev tools"
+        technologiesObject={devToolsObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="Deployment tools"
+        technologiesObject={deploymentToolsObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="Version control"
+        technologiesObject={versionControlObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="CMS"
+        technologiesObject={CMSOBject}
+      />
+      <TechnologyGroup
+        technologyGroupText="Design"
+        technologiesObject={designObject}
+      />
+      <TechnologyGroup
+        technologyGroupText="Learning roadmap"
+        technologiesObject={learningRoadmapObject}
+      />
     </section>
-  )
-}
+  );
+};
 
-module.exports = Technologies
+module.exports = Technologies;
